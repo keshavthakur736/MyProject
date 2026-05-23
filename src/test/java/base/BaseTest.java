6c7c9d0dd8b3d89759f0162a6fdc68d9f0c51f86
keshavthakur736 <keshavthakur736@gmail.com>
2026-05-23 13:30:28 +0530

    hi keshav

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,18 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+
+import java.sql.Driver;
+
+public class BaseTest {
+    protected WebDriver getDriver(){
+        return driver;
+    }
+    BeforeClass
+    public void setup(){
+        //Log.info
+
+    }
+
+}
