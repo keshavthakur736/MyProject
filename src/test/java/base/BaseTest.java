48ade0cdfcc689df606d4a6d21491859b3af4983
keshavthakur736 <keshavthakur736@gmail.com>
2026-05-25 13:30:42 +0530

    basetest added

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,18 +1,38 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import java.sql.Driver;
+import java.time.Duration;
 
 public class BaseTest {
-    protected WebDriver getDriver(){
+protected WebDriver driver;
+
+    public WebDriver getDriver(){
         return driver;
     }
-    BeforeClass
+    @BeforeMethod
     public void setup(){
-        //Log.info
+        //Log.info("launching Chrome...");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Open base URL here
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
     }
+    @AfterMethod
+    public void tearDown(){
+        if (driver != null){
+            //Log.info("Closing browser...");
+            driver.quit();
+        }
+    }
+
 
 }
