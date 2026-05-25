d7aec7d987d9cbbd9c56649f70bcd7c8f85a756c
keshavthakur736 <keshavthakur736@gmail.com>
2026-05-25 13:34:39 +0530

    basetest changed

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,10 +3,7 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-
-import java.sql.Driver;
 import java.time.Duration;
 
 public class BaseTest {
